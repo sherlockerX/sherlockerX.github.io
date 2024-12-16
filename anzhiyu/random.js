882227f513d5e8042d2e3428eb3a71074f244d8d
Yixing GU <77688784@qq.com>
2024-12-16 17:03:53 +0800

    Site updated: 2024-12-16 17:03:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["the.html","保姆级刷客教程.html"];function toRandomPost(){
+var posts=["保姆级刷客教程.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
